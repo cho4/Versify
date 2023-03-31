1285776de2956eb7a11be935debd49dbeeb9cbef
cho4 <eugenecho0425@gmail.com>
2023-03-31 15:24:20 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 """something"""
 from lyricsgenius import Genius
 import cohere
-from discography import Discography, Song
+from discography import Discography
+from sqlite3 import connect, Cursor
 
-genius_apikey = '2YNVCh64u2_spmTHbvnNAob6wyXkTwe7yB90UsERleejLKwafWbGAl_R-TWK8pL8'
 cohere_apikey = 'qiqWWzNgpO0oJ5pzLu21ETOuC37beHi6ON0XaigM'
-
-genius = Genius(genius_apikey)
 co = cohere.Client(cohere_apikey)
 
 
 def generate_discography(artist_name: str) -> Discography:
     """
     """
-    artist = genius.search_artist(artist_name, max_songs=3)
-    songs = artist.songs
-
-    discography = Discography(artist.name)
+    curr = connect_to_database()
+    songs = get_songs(artist_name, curr)
+    discography = Discography(artist_name)
 
     for song in songs:
-        title = song.title
-        lyrics = song.lyrics
+        title = song[0]
+        lyrics = song[1]
         embedding = co.embed([lyrics]).embeddings[0]
-
         discography.add_song(title, lyrics, embedding)
 
+
+
     return discography
+
+def connect_to_database() -> Cursor:
+    """Returns the cursor"""
+    conn = connect('lyrics_ds.db')
+    curr = conn.cursor()
+    return curr
+
+
+def get_songs(artist_name: str, curr: Cursor) -> list[tuple[str, str]]:
+    """
+    """
+    curr.execute('SELECT title, lyrics FROM songs WHERE artist = ?', (artist_name,))
+    return curr.fetchall()
