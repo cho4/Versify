0cadf67c2370bbfad4557bd2fd20d5ac424fa9b2
cho4 <eugenecho0425@gmail.com>
2023-03-31 21:49:30 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import cohere
 from discography import Discography, Song
 from sqlite3 import connect, Cursor, Connection
 import openai
+import tiktoken
 
 cohere_apikey = 'qiqWWzNgpO0oJ5pzLu21ETOuC37beHi6ON0XaigM'
-openai.api_key = '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'
+openai.api_key = ''
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 model_engine = "gpt-3.5-turbo"
 co = cohere.Client(cohere_apikey)
 
@@ -18,9 +20,9 @@ def generate_discography(artist_name: str) -> Discography:
     songs = get_songs(artist_name, curr)
     discography = Discography(artist_name)
 
-    for song in songs:
-        title = song[0]
-        lyrics = song[1]
+    for i in range(min(100, len(songs))):
+        title = songs[i][0]
+        lyrics = songs[i][1]
         embedding = co.embed([lyrics]).embeddings[0]
         discography.add_song(title, lyrics, embedding)
 
@@ -29,23 +31,43 @@ def generate_discography(artist_name: str) -> Discography:
     conn.close()   # Close the connection to the database
     return discography
 
-def generate_song_title(discography: Discography) -> str:
+def generate_song_title(lyrics: str) -> str:
     """"""
-    return 'Hi my name is Joe'
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a song title generator based on given song lyrics"},
+            {"role": "user", "content": f"Create an appopriate song title for these lyrics: {lyrics}"},
+        ]
+    )
+
+    title = response.choices[0]['message']['content']
+    return title
 
 def generate_song(discography: Discography) -> str:
     """Uses ChatGPT API to generate a return a new song
     lyric using the given songs lyrics as prompts for the generation
     """
-
     song_prompts = discography.top_five_songs()
+    lyrics = ''
+
+    for song in song_prompts:
+        lyrics += f'----------{song.title}----------\n'
+        lyrics += song.lyrics
+
+    num_tokens = len(encoding.encode(lyrics))
+    # if num_tokens >
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You generate lyrics of a song in the style "
                                           "of example songs that you are given."},
-            {"role": "user", "content": f"Write song lyrics in the style of this song: {song_prompts[0].lyrics}"}
+            {"role": "user", "content": f"Write a unique and original song lyrics in a similar style to that "
+                                        f"of the following songs: {lyrics}. Ensure that the lyrics are completely"
+                                        f"original! Don't reuse phrasing from the given lyrics. Remove any additional"
+                                        f"text that is not a part of the lyrics!"},
         ]
     )
 
